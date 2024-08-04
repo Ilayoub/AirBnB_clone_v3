94f455996a093bb74357cd4bd62434363d883e8d
Ilayoub <Jrindou.ayoub@gmail.com>
2024-08-04 20:45:11 +0000

    State

---
+++ api/v1/views/states.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""State file"""
+
+from models.base_model import BaseModel
+from models import storage
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def all_states():
+    """doc fuction"""
+    states_list = []
+    states = storage.all(State).values()
+    for state in states:
+        states_list.append(state.to_dict())
+    return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """get states"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404, 'Not found')
+    else:
+        return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """ doc delete"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    else:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """doc create"""
+    request_data = request.get_json()
+    if not request_data:
+        abort(400, 'Not a JSON')
+    if 'name' not in request_data:
+        abort(400, 'Missing name')
+    new_state = State(**request_data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """doc update"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    request_data = request.get_json()
+    if not request_data:
+        abort(400, 'Not a JSON')
+    if 'name' in request_data:
+        state.name = request_data['name']
+        storage.save()
+    return jsonify(state.to_dict()), 200
